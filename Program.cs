0491f476ed2f30951da41eedcbb0e335f659078a
niko057 <nicatnamazov57.nn@gmail.com>
2024-03-07 11:15:48 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+//Tapşırıq: 2 array verilmişdir, 2ci arraydəki bütün elementlər həm də 1cidə varsa , true əks halda false çap olunsun.
+
+int[] arr1 = { 211, 9, -6, 12, 34, 56, 32, 5, 19 };
+
+int[] arr2 = { 20, 23 };
+
+       bool check = false;
+
+
+
+for (int i = 0; i < arr2.Length; i++)
+{
+
+
+    for (int j = 0; j < arr1.Length; j++)
+    {
+        if (arr2[i] == arr1[j])
+
+        {
+            check = true;
+           
+           
+           
+        }
+
+        
+
+    }
+   
+}
+Console.WriteLine(check);
+
